c36e6fe1b1e0d5ff8da51acd5691b7d0d9ee8c0b
dibyajeetk <dibyajeet.creates@gmail.com>
2025-06-28 20:17:27 +0530

    feat: Rolled back to static grid size
    - using 600 x 600 px artboard for
    rendering grid.
    - dynamic grid requires calculating cell size live.
    
    dated: June 28, 2025
    time: 20:21 IST

--- src/script.js
+++ src/script.js
@@ -1,12 +1,23 @@
-let gridCount = 10;
+let gridCount = 20;
 const artBoardUI = document.querySelector("#artboard");
 
+// store gridCount
+// get length of any side of the #artboard. 
+const artboardSize = artBoardUI.getBoundingClientRect().width; // this is static
+// #cell size = #artboard side(600px) / #gridCount(10). === 600 / 10 => 60px
+let cellSize = artboardSize / gridCount;
+// total number of box = grid count ** 2;
+let cellTotal = gridCount ** 2;
+// run a for loop to create necessary divs(cellUI)
 
-for (let i = 0; i <= gridCount; i++) {
+
+for (let i = 0; i < cellTotal; i++) {
     
     const cellUI = document.createElement("div")
     cellUI.setAttribute("class", "cell");
+    cellUI.style.width = `${cellSize}px`;
+    cellUI.style.height = `${cellSize}px`;
     artBoardUI.appendChild(cellUI);
 
-    // console.log(cellUI)
+    console.log(cellUI)
 }
